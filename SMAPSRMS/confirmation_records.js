d7340b539a9afb8584ce66984caec428b4a06c17
Derek Jaleel <derekanc@gmail.com>
2025-04-28 22:53:10 +0800

    Delete SMAPSRMS directory

--- SMAPSRMS/confirmation_records.js
+++
@@ -1,9 +0,0 @@
-// Function to go back to the previous page
-function goBack() {
-    window.history.back();
-}
-
-// Function to close the current window
-function closeWindow() {
-    window.close();
-}
--- SMAPSRMS/marriage.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const closeButton = document.querySelector('.close-btn');
-    closeButton.addEventListener('click', function() {
-        window.close(); // Close the form when the close button is clicked
-    });
-
-    // Additional functionality can be added here
-});
